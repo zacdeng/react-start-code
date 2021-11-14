ee8e5274f6d4dcc412032f8b438508ae97306e69
zacdeng <zacdeng0720@gmail.com>
2021-11-14 16:45:49 -0600

    start code for react : Tic tac toe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,104 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+
+function Square(props) {
+    return (
+      <button className="square"
+              onClick={() => props.onClick()}>
+        {props.value}
+      </button>
+    );
+}
+
+class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true,
+        }
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        if(calculateWinner(squares) || squares[i])
+            return;
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: ! this.state.xIsNext,
+        });
+    }
+
+    renderSquare(i) {
+        return (
+            <Square value={this.state.squares[i]}
+                    onClick={() => this.handleClick(i)}/>
+        );
+    }
+
+  render() {
+      const winner = calculateWinner(this.state.squares);
+      let Status;
+      if(winner) {
+          Status = "Winner: " + winner;
+      }else {
+          Status = "Next Player: " + (this.state.xIsNext ? "X" : "O");
+      }
+
+    return (
+        <div>
+          <div className="status">{Status}</div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+    );
+  }
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Game-Board">
+        <Board />
+      </div>
+      <div className="Game-Info">
+        <div>{/* status */}</div>
+        <ol>{/* TODO */}</ol>
+      </div>
     </div>
   );
 }
